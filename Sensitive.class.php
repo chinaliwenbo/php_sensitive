aaa546aeb14f64e4d4e30db565d83ca3ec279bf7
liwb <liwenbo_csu@126.com>
2017-10-12 18:06:59 +0800

    Update Sensitive.class.php
    优化索引树缓存方案

--- Sensitive.class.php
+++ Sensitive.class.php
@@ -8,7 +8,6 @@
  * Date: 2017/10/10
  * Time: 19:28
  */
-
 class Sensitive{
 
     /**
@@ -26,6 +25,8 @@ class Sensitive{
     public static $_ACTION_REPLACE = 2;
     public static $_ACTION_ISLEGAL = 3;
 
+    public static $instance = null;
+
     /**
      * 获取敏感词库，可以自己写，比如去数据库获取敏感词等等
      * 这里默认从本类库自带的文本中读取,是测试用的敏感词
@@ -41,28 +42,30 @@ class Sensitive{
      * @param $content  需要处理的内容
      * @param int $type 操作类型， 1-查找敏感词，返回数组 array 2-替换敏感词，返回替换后的内容 string  3-返回是否包含敏感词 bool
      * @param string $replaceStr  如果是替换操作，可以把敏感词替换成该字符
-     * @param int $matchType 匹配或替换敏感词的个数
+     * @param int $matchType 匹配模式， 0-最长匹配 1-最短匹配
      * @throws Exception
      */
-    public static function filter($content, $type = 1, $replaceChar = '', $matchType = 1){
+    public static function filter($content, $type = 1, $replaceChar = '', $matchType = 0){
         //初始化
         require_once __DIR__ . "/sensitive/SensitiveHelper.php";
         require_once __DIR__ . "/sensitive/HashMap.php";
-        $instance = new SensitiveHelper();
-        //设置缓存文件和缓存时长
-        $instance->setCache(self::$CACHE_FILE, self::$CACHE_TIME);
-        //生成关键字查询索引
-        $instance->setTree( self::getSensitiveWord() );
+        if(!(self::$instance instanceof SensitiveHelper)){
+            self::$instance = new SensitiveHelper();
+            //设置缓存文件和缓存时长
+            self::$instance->setCache(self::$CACHE_FILE, self::$CACHE_TIME);
+            //生成关键字查询索引
+            self::$instance->setTree();
+        }
 
         //进行操作
         switch ($type){
             case 2: //直接替换敏感字符，返回替换后的字符串
-                return $instance->replace($content, $replaceChar , '', '', $matchType);
+                return self::$instance->replace($content, $replaceChar , '', '', $matchType);
             case 3: //查找是否包含有，返回bool
-                return $instance->islegal($content);
+                return self::$instance->islegal($content);
             case 1: //查找出敏感字符，返回敏感字符数组
             default:
-                return $instance->getBadWord($content, $matchType);
+                return self::$instance->getBadWord($content, $matchType);
         }
     }
 }
